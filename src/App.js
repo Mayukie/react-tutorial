3727792d71dbc1eabbb1cd2595e08d397a977128
Supranee Thaskhwan <qstcxi@gmail.com>
2025-05-12 16:14:50 +0700

    state tutorial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const name = "yuki";
+
+  let count = 0;
+  function addCount() {
+    count++;
+    console.log(count);
+  }
+
+  // useState
+  const [countState, setCountState] = useState(0);
+  function addCountState() {
+    setCountState(countState + 1);
+  }
+  function minusCountState() {
+    setCountState(countState - 1);
+  }
+
+  // array state
+  // [{id, name},{},{},...]
+  const initialStudents = [
+    { id: 1, name: "Emi" },
+    { id: 2, name: "Bonnie" },
+  ];
+  const [students, setStudent] = useState(initialStudents);
+  function deleteStudent(id) {
+    setStudent(students.filter((item) => item.id != id));
+  }
+  const [show, setShow] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello world: {name}</h1>
+      <button onClick={() => alert("Hello React")}>click</button>
+
+      {/* button */}
+      <h1>{count}</h1>
+      <button onClick={addCount}>add+1</button>
+
+      {/* state */}
+      <h1>useState</h1>
+      <h1>{countState}</h1>
+      <button onClick={addCountState}>add</button>
+      <button onClick={minusCountState}>minus</button>
+      <button onClick={() => setCountState(0)}>reset</button>
+
+      <h1>Student info = {students.length}</h1>
+      <button onClick={() => setShow(!show)}>show</button>
+      <ul>
+        {show == true && students.map((item) => (
+          <li key={item.id}>
+            <p>
+              {item.id} - {item.name}
+            </p>
+            <button onClick={() => deleteStudent(item.id)}>delete</button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
